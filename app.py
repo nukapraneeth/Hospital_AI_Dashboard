fb0955092d7ecb94327ed7bed4b5388fe2243ab8
nukapraneeth <nukapraneeth37@gmail.com>
2026-06-26 20:22:28 +0530

    Made patient table dynamic using Flask and Jinja

--- app.py
+++ app.py
@@ -7,6 +7,35 @@ def home():
 
     prediction = "No prediction yet."
 
+    patients = [
+    {
+        "id": "001",
+        "name": "Rahul Sharma",
+        "age": 25,
+        "disease": "Flu",
+        "doctor": "Dr. Smith",
+        "status": "Admitted"
+    },
+
+    {
+        "id": "002",
+        "name": "Priya Patel",
+        "age": 41,
+        "disease": "Diabetes",
+        "doctor": "Dr. Johnson",
+        "status": "Stable"
+    },
+
+    {
+        "id": "003",
+        "name": "Arjun Kumar",
+        "age": 58,
+        "disease": "Heart Disease",
+        "doctor": "Dr. Williams",
+        "status": "Critical"
+    }
+    ]
+
     if request.method == "POST":
 
         symptoms = request.form.get("symptoms", "").lower()
@@ -25,8 +54,9 @@ def home():
 
     return render_template(
         "index.html",
-        prediction=prediction
-    )
+        prediction=prediction,
+        patients=patients    
+        )
 
 if __name__ == "__main__":
     app.run(debug=True)
